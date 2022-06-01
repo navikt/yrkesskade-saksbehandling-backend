0ba0a96c0252fafa3d243155710ff7b307def6c3
Jone Lura <jone.lura@gmail.com>
2022-06-01 06:45:23 +0200

    YSMOD-361
    - lagt til unleash konfigurasjon

---
+++ src/main/kotlin/no/nav/yrkesskade/saksbehandling/config/ApplicationConfig.kt
@@ -0,0 +1,10 @@
+package no.nav.yrkesskade.saksbehandling.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@ConfigurationPropertiesScan
+@Configuration
+class ApplicationConfig {
+}
