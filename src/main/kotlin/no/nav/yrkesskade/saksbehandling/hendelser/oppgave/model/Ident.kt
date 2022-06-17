5d78e0fcd0976fdbb64eacb6af7ce5e6ba848e9e
Erik Prøis Olsnes <erik.prois.olsnes@nav.no>
2022-06-17 08:31:20 +0200

    YSMOD-328 * Fjernet Ident.kt * La til "behandlingsresultat" - enum, kolonne, entity-felt

--- src/main/kotlin/no/nav/yrkesskade/saksbehandling/hendelser/oppgave/model/Ident.kt
+++
@@ -1,6 +0,0 @@
-package no.nav.yrkesskade.saksbehandling.hendelser.oppgave.model
-
-class Ident (
-    val verdi: String,
-    val folkeregisterident: String,
-)
---
+++ src/main/kotlin/no/nav/yrkesskade/saksbehandling/model/Behandlingsresultat.kt
@@ -0,0 +1,8 @@
+package no.nav.yrkesskade.saksbehandling.model
+
+enum class Behandlingsresultat {
+    INNVILGELSE,
+    AVSLAG,
+    DELVIS_INNVILGELSE,
+    RA_BEHANDLET_UTFOERT
+}
