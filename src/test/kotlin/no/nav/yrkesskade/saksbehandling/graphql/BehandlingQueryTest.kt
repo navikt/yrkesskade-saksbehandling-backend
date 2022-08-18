b3bd39178a7200263c46a88e0b7d3b9b9962c6bc
Jone Lura <jone.lura@gmail.com>
2022-08-18 10:18:40 +0200

    YSMOD-487
    - ny spørring som bruker autentisert bruker for å henter ut brukernavn

--- src/test/kotlin/no/nav/yrkesskade/saksbehandling/graphql/BehandlingQueryTest.kt
+++ src/test/kotlin/no/nav/yrkesskade/saksbehandling/graphql/BehandlingQueryTest.kt
@@ -2,8 +2,11 @@ package no.nav.yrkesskade.saksbehandling.graphql
 
 import com.graphql.spring.boot.test.GraphQLTest
 import com.graphql.spring.boot.test.GraphQLTestTemplate
+import no.nav.security.mock.oauth2.MockOAuth2Server
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import no.nav.yrkesskade.saksbehandling.config.GraphQLScalarsConfig
 import no.nav.yrkesskade.saksbehandling.repository.BehandlingRepository
+import no.nav.yrkesskade.saksbehandling.security.AutentisertBruker
 import no.nav.yrkesskade.saksbehandling.test.AbstractTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -12,7 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
 
-@Import(GraphQLScalarsConfig::class)
+@Import(value = [GraphQLScalarsConfig::class])
 @GraphQLTest
 class BehandlingQueryTest : AbstractTest() {
 
@@ -22,6 +25,9 @@ class BehandlingQueryTest : AbstractTest() {
     @MockBean
     lateinit var behandlingRepository: BehandlingRepository
 
+    @MockBean
+    lateinit var autentisertBruker: AutentisertBruker
+    
     @Test
     fun `hent antall behandlinger`() {
         Mockito.`when`(behandlingRepository.count()).thenReturn(1)
