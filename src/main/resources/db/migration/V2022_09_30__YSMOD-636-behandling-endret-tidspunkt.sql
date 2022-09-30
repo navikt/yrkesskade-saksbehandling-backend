6564eeefe728ede8cfc9479be69aac4b51d2bc52
Erlend Kristiansen <erlend.kristiansen@nav.no>
2022-09-30 11:27:20 +0200

    YSMOD-636 Fjern fylling av verdier i endret_tidspunkt

--- src/main/resources/db/migration/V2022_09_30__YSMOD-636-behandling-endret-tidspunkt.sql
+++ src/main/resources/db/migration/V2022_09_30__YSMOD-636-behandling-endret-tidspunkt.sql
@@ -1,4 +1,1 @@
 ALTER TABLE behandling ADD COLUMN endret_tidspunkt TIMESTAMP;
-
--- Vi har ikke tidspunktet for siste endring, så bruker opprettet tidspunkt for å sette en verdi på behandlinger som er endret
-UPDATE behandling SET endret_tidspunkt = opprettet_tidspunkt where status <> 'IKKE_PAABEGYNT';
